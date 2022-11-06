fda6db2d1c28553513ee2c5ecdb478d2b6404b22
ZZT897 <897239316@qq.com>
2022-11-06 14:56:26 +0800

    feat: 管理后台新闻资讯模块开发完成

--- frontend/news.js
+++ frontend/news.js
@@ -15,4 +15,40 @@ router.get('/getNewsData', (req, res) => {
   })
 })
 
+router.post('/delNewsData', (req, res) => {
+  const { id } = req.body;
+  let sql = `delete from newsdata where id=${id}`;
+  connection.query(sql, (err, data) => {
+    if (!err) {
+      res.send({ code: 200, message: '删除新闻资讯成功' })
+    } else {
+      res.send({ code: 403, message: '删除新闻资讯失败' })
+    }
+  })
+})
+
+router.post('/editNewsData', (req, res) => {
+  const { id, title, url } = req.body;
+  let sql = `update newsdata set title='${title}', url='${url}' where id=${id}`;
+  connection.query(sql, (err, data) => {
+    if (!err) {
+      res.send({ code: 200, message: '修改新闻资讯成功' });
+    } else {
+      res.send({ code: 403, message: '修改新闻资讯失败' });
+    }
+  })
+})
+
+router.post('/addNewsData', (req, res) => {
+  const { title, url } = req.body;
+  let sql = `insert into newsdata(title, url) values('${title}', '${url}')`;
+  connection.query(sql, (err, data) => {
+    if (!err) {
+      res.send({ code: 200, message: '新增新闻资讯成功' });
+    } else {
+      res.send({ code: 403, message: '新增新闻资讯失败' });
+    }
+  })
+})
+
 export default router
